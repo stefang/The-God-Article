309760b25ea7babe03d5a2c224724c7edbf85f59
Stefan Goodchild <me@stefangoodchild.com>
2014-05-07 00:25:03 +0100

    Init Commit

---
+++ testRecorder/DerivedData/testRecorder/Build/Intermediates/testRecorder.build/Debug/testRecorder.build/Objects-normal/i386/kiss_fft.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/stefg/Documents/Visuals/00_oF/of_v0.8.1_osx_release/apps/myApps/testRecorder/../../../addons/ofxFft/libs/kiss/kiss_fft.c \
+  ../../../addons/ofxFft/libs/kiss/_kiss_fft_guts.h \
+  ../../../addons/ofxFft/libs/kiss/kiss_fft.h
---
+++ testRecorder/DerivedData/testRecorder/Build/Intermediates/testRecorder.build/Debug/testRecorder.build/Objects-normal/i386/tinyxml.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/stefg/Documents/Visuals/00_oF/of_v0.8.1_osx_release/apps/TheGodComplex/testRecorder/../../../addons/ofxXmlSettings/libs/tinyxml.cpp \
+  ../../../addons/ofxXmlSettings/libs/tinyxml.h
