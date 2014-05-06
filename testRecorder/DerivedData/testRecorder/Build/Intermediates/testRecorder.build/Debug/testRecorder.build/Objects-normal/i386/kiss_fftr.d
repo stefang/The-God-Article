1ca0d5f409e577cfeb9fdd71162ff6bcdfa1b887
Stefan Goodchild <me@stefangoodchild.com>
2014-05-07 00:46:18 +0100

    Removed derived data, added in local copy of addon

--- testRecorder/DerivedData/testRecorder/Build/Intermediates/testRecorder.build/Debug/testRecorder.build/Objects-normal/i386/kiss_fftr.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/stefg/Documents/Visuals/00_oF/of_v0.8.1_osx_release/apps/TheGodComplex/testRecorder/../../../addons/ofxFft/libs/kiss/kiss_fftr.c \
-  ../../../addons/ofxFft/libs/kiss/kiss_fftr.h \
-  ../../../addons/ofxFft/libs/kiss/kiss_fft.h \
-  ../../../addons/ofxFft/libs/kiss/_kiss_fft_guts.h
