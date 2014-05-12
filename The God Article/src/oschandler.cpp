c5a13e578080a28937bbd635282e75133f641f52
Stefan Goodchild <me@stefangoodchild.com>
2014-05-12 12:14:00 +0100

    Custom bits and bobs… Organising the app, adding some new bits

---
+++ The God Article/src/oschandler.cpp
@@ -0,0 +1,9 @@
+//
+//  oschandler.cpp
+//  The God Article
+//
+//  Created by Stefan Goodchild on 12/05/2014.
+//
+//
+
+#include "oschandler.h"
---
+++ The God Article/src/oschandler.h
@@ -0,0 +1,14 @@
+//
+//  oschandler.h
+//  The God Article
+//
+//  Created by Stefan Goodchild on 12/05/2014.
+//
+//
+
+#ifndef __The_God_Article__oschandler__
+#define __The_God_Article__oschandler__
+
+#include "ofMain.h"
+
+#endif /* defined(__The_God_Article__oschandler__) */
